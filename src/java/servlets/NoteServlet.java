e3599d1079f16565354a3aaefe5303f1ccfcc021
DWEI <DWEI@DESKTOP-2UFRABK>
2021-06-08 22:41:11 -0600

    Second commit doGet method done, javabean done

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -1,24 +1,42 @@
 package servlets;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+import models.Note;
 
 /**
  *
  * @author DWEI
  */
 public class NoteServlet extends HttpServlet {
-
-   
-
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        //try catch to read the note.txt file and use its contents to create a note object
+        //use the note object to show contents on jsp
+        try{
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        String title = br.readLine();
+        String contents = br.readLine();
+        while(br.readLine() != null){
+            contents = contents + br.readLine();
+        }
+
+        Note note = new Note(title, contents);
+        request.setAttribute("note", note);
+        
+        
+        }catch(IOException e){
+            System.err.println("File cannot be read");
+        }
+        
+        //this will display the view note JSP as a view
+        getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
     }
 
     
--- web/WEB-INF/viewnote.jsp
+++ web/WEB-INF/viewnote.jsp
@@ -9,9 +9,17 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>view note page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Simple Note Keeper</h1>
+        <form method="post">
+        <h2>View Note</h2>
+        
+        <p><b>Title:</b> ${note.title}</p>
+        <p><b>Contents:</b> <br> ${note.contents}</p>
+        
+        <a href="note?edit">Edit</a>
+        </form>
     </body>
 </html>
